09cfe2980042a4b8510a632b7c7c8b110e4989e2
Ernesto Bessone <ejubessone@gmail.com>
2021-11-26 11:49:44 -0300

    modificacion general

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -11,11 +11,12 @@ function CartContextProvider({children}){
     const [cartList, setCartList] = useState([])
 
     const agregarItem=(newItem, newQuantity, mostrar, setMostrar)=>{
-        const evitarDuplicado = cartList.find((item) => item.item.id === newItem.id);
+        const evitarDuplicado = cartList.find((item) => {
+            return item.item.nombre === newItem.nombre});
         let newCart;
         let qty;
         if(evitarDuplicado) {
-            newCart = cartList.filter((item) => item.item.id !== newItem.id);
+            newCart = cartList.filter((item) => item.item.nombre !== newItem.nombre);
             qty = evitarDuplicado.quantity + newQuantity
         } else {
             newCart = [...cartList];
@@ -26,7 +27,7 @@ function CartContextProvider({children}){
         };
 
     const removerItem = (itemId) =>{
-        let filtrarCarrito = cartList.filter((item) => item.item.id !== itemId);
+        let filtrarCarrito = cartList.filter((item) => item.item.nombre !== itemId);
             setCartList(filtrarCarrito);
         };
 
@@ -34,45 +35,58 @@ function CartContextProvider({children}){
         setCartList([])
     }
 
-    const crearOrdenDePedido = (orden) =>{
+    // const crearOrdenDePedido = () =>{
 
-        let precioTotal = 0
-        let items = []
+    //     orden.buyer = { nombre: "Ernesto", telefono: "3413605555", email: "ejubessone@hotmail.com"}
+    //     orden.total = precioTotal();
+    //     orden.items = cartList.map(cartItem => {
+    //         const id = cartItem.item.id;
+    //         const nombre = cartItem.item.nombre;
+    //         const precio = cartItem.item.precio;
 
-        const user = {
+    //         return {id, nombre, precio}
+    //     })   
+    
+
+     const crearOrdenDePedido = (orden) =>{
+
+         let precioTotal = 0
+         let items = []
+
+         const user = {
             name: 'Ernesto Bessone',
             phone: 3413605555,
             email: 'ejubessone@hotmail.com'
         }
 
         const itemEnLaOrden = (orden) => {
-            cartList.map(item => items.push({
-                id: item.item.id,
-                name: item.item.nombre,
-                price: item.item.precio,
+            cartList.map(itemCart => items.push({
+                id: itemCart.item.id,
+                name: itemCart.item.nombre,
+                price: itemCart.item.precio,
             }))
             return items
         }
 
-        const precio = () => {cartList.map(item => {
-            precioTotal += item.item.precio * item.cantidad
-            return precioTotal
-        })}
+      //  const precio = () => {cartList.map(item => {
+      //      precioTotal += item.item.precio * item.cantidad
+      //      return precioTotal
+      //  })};
 
         const db = getFirestore();
         const dbOrders = db.collection('ordenes');
-        precio()
-        
+       // precio()
+     
         dbOrders.add({
             comprador: user,
             item: itemEnLaOrden(orden),
             fecha: firebase.firestore.Timestamp.fromDate(new Date()),
-            total: precioTotal
+           // total: precioTotal
         })
         .then( res => alert(`Su orden de compra es: ${res.id}`))
-        .catch(err => console.log(err))
-
-        setCartList()
+        .catch(err => console.log(err));
+    
+        setCartList([])
     }
     
     console.log(cartList);    
